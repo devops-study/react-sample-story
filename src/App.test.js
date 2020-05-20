2c8d07f3956c9e5544eba47f304464c94f4d5266
devops-study <65510298+devops-study@users.noreply.github.com>
2020-05-20 13:40:42 +0900

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/DevOps学習用ページ/i);
   expect(linkElement).toBeInTheDocument();
 });
